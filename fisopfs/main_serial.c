401dff83955cfcca86f3bb0dd105f1571e19f7a4
Marcelo Origoni Agustin <marce.origoni@gmail.com>
2024-11-25 11:28:43 -0300

    se agrego algun test para ver

--- fisopfs/main_serial.c
+++ fisopfs/main_serial.c
@@ -2,8 +2,60 @@
 
 #include "fs/serial.h"
 #include <stdio.h>
+#include <sys/types.h>
+
+#include <string.h>
 #include <stdlib.h>
 #include "fs/inode.h"
+
+void showBytes(const char* buff, int count){
+    printf("count: %d 0x%02X(%u)",count, (unsigned char)*buff,(unsigned char)*buff);
+    
+    for(int ind = 1; ind< count; ind++){
+        char vl = *(buff+ind);
+        printf(", 0x%02X(%u)",(unsigned char)vl, vl);
+    }
+    printf("\n");
+}
+
+
+static void testUno(const char *filename){
+     int err = 0;
+     struct SerialFD writer = openWriter(filename, &err);
+     
+     writeInt(&writer, 25);
+     writeStr(&writer, "UNO");
+     writeInt(&writer, 50);
+     
+     closeWriter(&writer);
+     
+     
+
+     struct SerialFD reader = openReader(filename, &err);
+     closeWriter(&reader);
+     int out;
+     char * outStr = NULL;
+     
+     readInt(&reader, &out);
+     if(out != 25){
+         printf("---> FIRST NUM NO ERA 25 fue %d!\n", out);
+     }
+     
+     readStr(&reader, &outStr);
+     
+     if(outStr == NULL){
+         printf("-> out str era null\n");
+     } else if(strcmp("UNO", outStr) != 0){
+         printf("---> FIRST STR NO ERA 'UNO' fue '%s'\n", outStr);
+     }
+     
+     readInt(&writer, &out);
+     if(out != 50){
+         printf("---> SECOND NUM NO ERA 50! fue %d!\n", out);
+     }
+
+}
+
 
 static void saveInode(struct SerialFD* writer, struct Inode* inode){
     writeInt(writer, inode->id);
@@ -65,10 +117,21 @@ static int showData(const char * filename){
 int
 main(int argc, char *argv[])
 {
-     int ret = writeData(argv[1]);
-     if(ret != 0){
-         return ret;
-     }
+     int data = 512+ 254;
+     char* buff = "UN ELEMENTO!";
+     showBytes(buff, strlen(buff));
+     printf("----------\n");
+     buff = (char*) &data;
+     showBytes(buff, sizeof(data));
      
-     return showData(argv[1]);
+     
+     testUno(argv[1]);
+     
+     return 0;
+     //int ret = writeData(argv[1]);
+     //if(ret != 0){
+     //    return ret;
+     //}
+     
+     //return showData(argv[1]);
 }
