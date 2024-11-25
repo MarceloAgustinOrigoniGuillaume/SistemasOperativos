12bc371de8f63f640692d33354c9c31040aa0539
melina-r <mretamozo@fi.uba.ar>
2024-11-25 17:46:56 -0300

    implemento base

--- fisopfs/fs/directories.c
+++ fisopfs/fs/directories.c
@@ -3,8 +3,6 @@
 #include <stdio.h>
 #include <string.h>
 
-
-
 static int exists_def = 0; 
 
 // Libera al directorio, recursivamente
@@ -17,13 +15,13 @@ struct Inode* rmChild(struct Inode* root, const char* path){ // Persona 2
     printf("RM CHILD root %s rel: %s\n",root->name, path);
     return NULL;
 }
+
 // Agrega el inodo hijo como hijo al padre.
 int addChild(struct Inode* parent, struct Inode* child){ // Persona 2
     printf("ADD CHILD parent %s child: %s\n",parent->name, child->name);
     //-ENOENT
     return 0;
 }
-
 
 struct Inode* searchRelative(const struct Inode* root,const char* path){ // Persona 2
     printf("LOOK FOR %s\n ", path);
@@ -39,8 +37,8 @@ struct Inode* searchRelative(const struct Inode* root,const char* path){ // Pers
     return NULL;
 }
 
-
 char * NAME_DEF = "newfile";
+
 // Retorna el padre! Null si no existe un padre. name_child = Null en caso de que ya exista.
 struct Inode* searchNew(const struct Inode* root, const char* path, char ** name_child){ // Persona 2
     if(exists_def == 0 && strcmp(path, "/newfile") == 0){
@@ -53,8 +51,6 @@ struct Inode* searchNew(const struct Inode* root, const char* path, char ** name
     
     return NULL;
 }
-
-
 
 void readChildren(struct Inode* dir, struct DirEntries* out){ // Persona 2
     printf("Get Children of %s\n", dir->name);
@@ -71,3 +67,43 @@ void readChildren(struct Inode* dir, struct DirEntries* out){ // Persona 2
     }
     out->count = 0;
 }
+
+
+char** split(const char* str, const char* delimiter, int* count) {
+    char* temp = strdup(str); 
+
+    int parts = 0;
+    char* token = strtok(temp, delimiter);
+    while (token != NULL) {
+        parts++;
+        token = strtok(NULL, delimiter);
+    }
+    free(temp); 
+
+    char** result = malloc(parts * sizeof(char*));
+    if (!result) {
+        perror("Error reservando memoria");
+        exit(EXIT_FAILURE);
+    }
+
+    temp = strdup(str); 
+
+    token = strtok(temp, delimiter);
+    int i = 0;
+    while (token != NULL) {
+        result[i] = strdup(token);
+
+        if (!result[i]) {
+            perror("Error reservando memoria para token");
+            exit(EXIT_FAILURE);
+        }
+
+        i++;
+        token = strtok(NULL, delimiter);
+    }
+
+    free(temp); 
+    *count = parts;
+    return result;
+}
+
--- fisopfs/fs/directories.h
+++ fisopfs/fs/directories.h
@@ -1,20 +1,21 @@
 #ifndef DIRECTORIES_H
 #define DIRECTORIES_H
 
+#define INIT_DIR_ENTRIES 256
+
 #include "./inode.h"
 
 
 struct DirData{ // Persona 2
    int size; // Campo de Inode Data*, tiene que estar arriba de todo!
    int capacity;
+   struct DirEntries entries[INIT_DIR_ENTRIES];
 };
-
 
 struct DirEntries{
     int count;
     struct Inode* first; // id del inodo? o puede ser algo interno.
 };
-
 
 void serializeDirData(int fd_out, const struct DirData* data_out);    // Persona 2/1?
 void deserializeDirData(int fd_in, struct DirData* data);   // Persona 2/1?
@@ -29,14 +30,11 @@ struct Inode* searchRelative(const struct Inode* root, const char* path); // Per
 // Si ya existe o no se permite. name_child == NULL
 struct Inode* searchNew(const struct Inode* root, const char* path, char ** name_child); // Persona 2
 
-
 // Para el ls, lista los hijos en el struct out.
 void readChildren(struct Inode* dir, struct DirEntries* out); // Persona 2
 
-
 // Libera al directorio, recursivamente
 void freeDir(struct Inode* dir); // Persona 2
-
 
 // Busca al inodo, y lo remueve del padre. Retorna el inodo hijo. Para su posterior liberacion de hacer falta.
 struct Inode* rmChild(struct Inode* root, const char* path); // Persona 2
